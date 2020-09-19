540875d2a9be0df52d725c9a691ec82b7e91e290
shaonkabir8 <shaonkabir98@gmail.com>
2020-09-19 09:39:54 +0600

    refactor: update site-metaData

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/shaon/work/gatsby-blog/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/shaon/work/gatsby-blog/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/shaon/work/gatsby-blog/src/pages/index.js"))),
   "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/home/shaon/work/gatsby-blog/src/pages/using-typescript.tsx"))),
